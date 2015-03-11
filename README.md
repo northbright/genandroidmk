ee2e30a2c0c696b6af1bce33a82807ae4681ce6f
Frank Xu <frank.xu@northbright.com>
2015-03-11 14:30:30 +0800

    Fix the issue that LOCAL_MODULE_TARGET_ARCH should be set correctly

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # genandroidmk
 
-#### Description
+#### Integrate Prebuilt Apps
 
 ###### Before Android 5.0 L  
 To integrate prebuilt apps which contains native libraries, you need to:  
@@ -13,5 +13,32 @@ There's no need to extract the libraries in APK and copy them to BSP.
 Android will extract the libs and copy them to `/system/app/APP_NAME/lib` automatically while building the system image.  
 We only need to specify the prebuilt libs(`LOCAL_PREBUILT_JNI_LIBS`) in `Android.mk`.  
 
-`genandroidmk` is a tool that help to generate `LOCAL_PREBUILT_JNI_LIBS` in `Android.mk`.  
+#### About This Tool
+`genandroidmk` is a tool that help to generate `LOCAL_PREBUILT_JNI_LIBS` and the whole `Android.mk` automatically.  
 It will check the `/lib` in APK and let users choose the CPU arch of libraries to fill the `LOCAL_PREBUILT_JNI_LIBS` variable.  
+
+#### How to Use
+
+    ./genandroidmk -i <input APK file> -n <app name>
+    Ex:
+    ./genandroidmk -i WeChat.apk -n WeChat
+
+###### Arguments:
+
+* `<input APK file>`  
+   abs path of APK and `Android.mk` will be outputed in the same folder
+* `<app name>`  
+   It will be used to set `LOCAL_MODULE` in `Android.mk`  
+   if it's not set, `LOCAL_MODULE` will be set to the APK name by default.
+
+###### Choose CPU Arch of native libraries:  
+If the APK contains multi-arch native libraries, you will be asked to select one arch:
+
+    Please select one of avaialbe arches in current APK:
+    ====================================
+    1: armeabi
+    2: armeabi-v7a
+    3: x86
+
+###### Output
+`Android.mk` will be put under the same folder as input APK file.
--- main.go
+++ main.go
@@ -2,35 +2,80 @@ package main
 
 import (
 	"archive/zip"
+	"bytes"
 	"flag"
 	"fmt"
 	"github.com/northbright/goutil"
+	"io/ioutil"
 	"path"
 	"regexp"
 	"sort"
 )
 
 var (
-	DEBUG   bool                = false
-	AppName string              = ""
-	ApkPath string              = ""
-	Arch    string              = "" // armeabi, armeabi-v7a, arm64-v8a, x86, x86_64
-	ApkDir  string              = ""
-	pattern string              = `^lib/(.*)/(.*\.so)$`     // find arch and .so name in zip file: FindStringSubmatch() should return [3]string, arrs[1] = arch, arrs[2] = .so name
-	libMap  map[string][]string = make(map[string][]string) // map to store libs. Key = arch, Value = lib slice
-	hasLibs bool                = false
+	DEBUG         bool                = false
+	AppName       string              = ""
+	ApkPath       string              = ""
+	Arch          string              = "" // armeabi, armeabi-v7a, arm64-v8a, x86, x86_64
+	ApkDir        string              = ""
+	AndroidmkPath string              = ""
+	pattern       string              = `^lib/(.*)/(.*\.so)$`     // find arch and .so name in zip file: FindStringSubmatch() should return [3]string, arrs[1] = arch, arrs[2] = .so name
+	libMap        map[string][]string = make(map[string][]string) // map to store libs. Key = arch, Value = lib slice
 )
+
+func genAndroidmk(file, apk, moduleName, arch string, libs []string) (err error) {
+	var buffer bytes.Buffer
+
+	buffer.WriteString("LOCAL_PATH := $(call my-dir)\n")
+	buffer.WriteString("\nmy_archs := arm x86 arm64\n")
+	buffer.WriteString("my_src_arch := $(call get-prebuilt-src-arch, $(my_archs))\n")
+	buffer.WriteString("\ninclude $(CLEAR_VARS)\n")
+	buffer.WriteString(fmt.Sprintf("LOCAL_MODULE := %s\n", moduleName))
+	buffer.WriteString("LOCAL_MODULE_CLASS := APPS\n")
+	buffer.WriteString("LOCAL_MODULE_TAGS := optional\n")
+	buffer.WriteString("LOCAL_BUILT_MODULE_STEM := package.apk\n")
+	buffer.WriteString("LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)\n")
+	buffer.WriteString("LOCAL_CERTIFICATE := PRESIGNED\n")
+	buffer.WriteString(fmt.Sprintf("LOCAL_SRC_FILES := %s\n", apk))
+
+	if len(libs) > 0 {
+		buffer.WriteString("\n")
+		buffer.WriteString("LOCAL_PREBUILT_JNI_LIBS := \\\n")
+		for i, v := range libs {
+			buffer.WriteString("  @")
+			buffer.WriteString(v)
+			if i != len(libs)-1 {
+				buffer.WriteString(" \\\n")
+			} else {
+				buffer.WriteString("\n")
+			}
+		}
+	}
+
+	buffer.WriteString("\nLOCAL_MODULE_TARGET_ARCH := $(my_src_arch)\n")
+	buffer.WriteString("\ninclude $(BUILD_PREBUILT)\n")
+
+	if err = ioutil.WriteFile(file, buffer.Bytes(), 0777); err != nil {
+		fmt.Printf("ioutil.WriteFile(%s) error: %s\n", file, err)
+		return err
+	}
+
+	return nil
+}
 
 func main() {
 	flag.StringVar(&ApkPath, "i", "", "input APK file. Ex: -i ./WeChat.apk")
 	flag.StringVar(&AppName, "n", "", "LOCAL_MODULE in Android.mk. If not set, it'll use APK's name. Ex: -n WeChat")
-	flag.StringVar(&Arch, "a", "", "Arch of libraries(/lib/xx of apk) to be used.\nIt can be these values: armeabi, armeabi-v7a, arm64-v8a, x86, x86_64. If not set, it'll use the first arch in /lib. Ex: -a armeabi-v7a")
 
 	flag.Parse()
 
 	fmt.Printf("AppName = %s\n", AppName)
 	fmt.Printf("ApkPath = %s\n", ApkPath)
-	fmt.Printf("Arch = %s\n", Arch)
+
+	if ApkPath == "" {
+		flag.PrintDefaults()
+		return
+	}
 
 	r, err := zip.OpenReader(ApkPath)
 	if err != nil {
@@ -45,7 +90,8 @@ func main() {
 	}
 
 	ApkDir = path.Dir(ApkPath)
-	fmt.Printf("ApkDir = %s\n", ApkDir)
+	AndroidmkPath = path.Join(ApkDir, "Android.mk")
+	fmt.Printf("Output Android.mk = %s\n", AndroidmkPath)
 
 	re := regexp.MustCompile(pattern)
 
@@ -59,13 +105,12 @@ func main() {
 				fmt.Printf("arch: %s\n", arch)
 				fmt.Printf("so: %s\n", so)
 			}
-			libMap[arch] = append(libMap[arch], so)
+			libMap[arch] = append(libMap[arch], f.Name)
 		}
 	}
 
 	// Check if it contains native libs(.so)
 	if len(libMap) != 0 {
-		hasLibs = true
 		arrs := []string{}
 
 		for k, _ := range libMap {
@@ -75,14 +120,41 @@ func main() {
 		if _, ok := libMap[Arch]; !ok { // input Arch argument is incorrect, get 1st arch
 			// sort arches by string
 			sort.Strings(arrs)
-			Arch = arrs[0]
 		}
 
-		fmt.Printf("hasLibs = %v, Arch = %s, libs:\n", hasLibs, Arch)
-		for _, v := range libMap[Arch] {
-			fmt.Printf("%s\n", v)
+		for {
+			index := 1
+			fmt.Printf("\nPlease select one of avaialbe arches in current APK:\n====================================\n")
+			for i, v := range arrs {
+				fmt.Printf("%d: %s\n", i+1, v)
+			}
+
+			// Wait user imput
+			if _, err := fmt.Scanf("%d", &index); err != nil {
+				fmt.Printf("fmt.Scanf() error: %s\n", err)
+				return
+			}
+
+			index -= 1
+			if 0 <= index && index < len(arrs) {
+				Arch = arrs[index]
+				fmt.Printf("You choice: %s, libs:\n", Arch)
+				for _, v := range libMap[Arch] {
+					fmt.Printf("%s\n", v)
+				}
+				break
+			} else {
+				fmt.Printf("Please input the right number: %d -- %d\n", 1, len(arrs))
+			}
 		}
 	} else { // no native libs
-		fmt.Printf("hasLibs = %v\n", hasLibs)
+		fmt.Printf("no native libs in APK\n")
 	}
+
+	fmt.Printf("Start gnerating %s\n", AndroidmkPath)
+	if err := genAndroidmk(AndroidmkPath, ApkPath, AppName, Arch, libMap[Arch]); err != nil {
+		fmt.Printf("genAndroidmk() error: %s\n", err)
+		return
+	}
+	fmt.Printf("Done\n")
 }
